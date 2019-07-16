745801eede3c6c74e66edcb8421f065b42c9d2e0
GraceSoftwares <51951120+GraceSoftwares@users.noreply.github.com>
2019-07-16 10:51:14 +0530

    Add files via upload

--- application/controllers/Controller.php
+++ application/controllers/Controller.php
@@ -6,7 +6,9 @@ class Controller extends CI_Controller{
     public function __construct()
     {
         parent::__construct();
-        $this->load->helper('url'); //Loading url helper    
+        $this->load->helper('url'); //Loading url helper  
+        $this->load->model('Countries');  
+        $this->load->library('pagination');
     } 
 
     public function index()
@@ -14,14 +16,86 @@ class Controller extends CI_Controller{
         //echo "Hello World";
         //$this->load->helper('url');
         $this->load->view('dashboard');
-    }  
+    }
+    
+    public function dashboard(){
+        $this->load->view('dashboardcontent');
+    }
+
+    private function dashData($tab){
+        /*$config = array();
+        $config["base_url"] = base_url() . "Controller/example1";
+        $config["total_rows"] = $this->Countries->record_count();
+        $this->pagination->initialize($config);
+        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;*/
+        $data["results"] = $this->Countries->
+            fetch_data($tab);
+        //$data["links"] = $this->pagination->create_links();
+        return $data;
+    }
+
+    //show booking table in dashboard
+    public function bookingtable(){
+        $this->load->view('bookingtable', $this->dashData('booking'));
+    }
+
+    //show checkin table in dashboard
+    public function checkintable(){
+        $this->load->view('checkintable',$this->dashData('checkin'));
+    }
+
+    //show checkout table in dashboard
+    public function checkouttable(){
+        $this->load->view('checkouttable',$this->dashData('checkout'));
+    }
+
+    private function reportData($selector){
+        $data["results"] = $this->Countries->getall($selector);
+        return $data;
+    }
+
+    public function upcoming(){
+        $this->load->view('upcoming',$this->reportData('upcoming'));
+    }
+
+    public function ongoing(){
+        $this->load->view('upcoming',$this->reportData('ongoing'));
+    }
+
+    public function completed(){
+        $this->load->view('upcoming',$this->reportData('completed'));
+    }
+
+    public function listall(){
+        $this->load->view('report');
+    }
+
+    public function regform(){
+        $this->load->view('regform');
+    }
+
+    public function presenter() {
+        $id = $this->uri->segment(3);
+        $data["results"] = $this->Countries->cust($id);
+        $this->load->view('presenter', $data);
+    }
+
+
     
     public function formPage(){
         $this->load->view('registrationform');
     }
 
-    public function dashboard(){
-        $this->load->view('dashboardcontent');
+    public function todayBooking(){
+        $this->load->view('todaybooking');
+    }
+
+    public function todayCheckin(){
+        $this->load->view('todayCheckin');
+    }
+
+    public function todayCheckout(){
+        $this->load->view('todayCheckout');
     }
 
 }
--- application/controllers/RestPostController.php
+++ application/controllers/RestPostController.php
@@ -12,15 +12,56 @@ class RestPostController extends REST_Controller {
         $this->load->model('FormModel', 'fm');
     }
 
-    function add_guest_post() {
-        $guest_noofguest = $this->post('guestcount');
-        $result = $this->fm->add_guest($guest_noofguest);
 
+    function getTax($customer_price, $customer_days){
+        $gstp = array(28, 18, 12);
+        $gsttax = array(7500, 2500, 1000);
+        $pricepd = ($customer_price/$customer_days);
+        for($i = 0; $i < sizeof($gsttax); $i++){
+            if($pricepd > $gsttax[$i]){
+                return $gstp[$i];
+            }
+        }
+        return 0;
+    }
+
+    function add_booking_post() {
+        $guest_id = uniqid();
+        $property_id = uniqid();
+        $customer_name = $this->post('name');
+        $customer_contact = $this->post('contact');
+        $customer_property = $this->post('selectedp');
+        $customer_roomtype = $this->post('roomtype');
+        $customer_roomno = $this->post('roomno');
+        $customer_checkIn = $this->post('checkIn');
+        $customer_checkOut = $this->post('checkOut');
+        $customer_gateway = $this->post('selectedName');
+        $customer_days = $this->post('days');
+        $customer_price = $this->post('price');
+        $customer_id_num = $this->post('id_num');
+        $customer_extra = $this->post('extra');
+        $customer_id_type = $this->post('id_type');
+        $customer_id_address = $this->post('id_address');
+        $customer_id_contact = $this->post('id_contact');
+        $customer_noofguest = $this->post('guest_count');
+        //$customer_sgstpct = 
+        $customer_cgstpct =  $this->getTax($customer_price, $customer_days);
+        $customer_cgstamt = $customer_price*($customer_cgstpct/100); 
+        $result = $this->fm->add_property($property_id, $customer_property, $customer_roomtype, $customer_roomno, $customer_price, $customer_extra, $customer_cgstpct, $customer_cgstamt);
+        if ($result === FALSE) {
+            $this->response(array('status' => 'failed'));
+        } 
+        $result = $this->fm->add_guest($guest_id, $customer_noofguest);
+        if ($result === FALSE) {
+            $this->response(array('status' => 'failed'));
+        }
+        $result = $this->fm->add_booking(uniqid(), $guest_id, $property_id, $customer_name, $customer_days, $customer_gateway, $customer_contact, $customer_checkIn, $customer_checkOut, $customer_price, $customer_extra);
         if ($result === FALSE) {
             $this->response(array('status' => 'failed'));
         } else {
             $this->response(array('status' => 'success'));
         }
     }
+
 
 }

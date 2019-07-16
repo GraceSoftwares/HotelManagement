5c32445eb65ba4a615fe9a8ba10e221a1bec7418
GraceSoftwares <51951120+GraceSoftwares@users.noreply.github.com>
2019-07-16 11:19:38 +0530

    Add files via upload

--- js/angularcontroller.js
+++ js/angularcontroller.js
@@ -1,10 +1,10 @@
 'use strict';
- 
-var app = angular.module('myForm').controller('formValidation',['$scope', 'angularservice', function($scope, angularservice) { 
+angular.module('myForm').controller('fetch',['$scope', 'angularservice', function($scope, angularservice) { 
+
 
 
     var self = this;
-    self.customer = {  name:'',contact:'', email:'', checkIn:'', checkOut:'', guest_count:'', booking_id:'', price:'', id_num:'' , id_type:'', id_address:'',id_contact:''};
+    self.customer = {  name:'',contact:'', email:'', selectedp:'', roomtype:'', roomno:'', checkIn:'', checkOut:'', guest_count:'', booking_id:'', price:'', id_num:'' , id_type:'', id_address:'',id_contact:''};
     self.submit = submit;
 
     function createBooking(guest){
--- js/dropdown.js
+++ js/dropdown.js
@@ -1,11 +1,33 @@
 'use strict';
-var app = angular.module('myForm',[]).controller('formValidation',['$scope', function($scope)
+angular.module('myForm',[]).controller('formValidation',['$scope', 'angularservice', function($scope, angularservice)
 {
+
     $scope.gateway= ["Walk in", "OYO", "Other"];
     $scope.nationality=["INDIAN", "FOREIGNER", "NRI"];
     $scope.names = ["Apollo", "Munnar", "Lake View", "Anna Nagar", "Park Town", "Le Grace MDU"];
-    $scope.change=function(){$scope.needId = ($scope.selectedName === $scope.gateway[0]) ? false : true;
+    $scope.change = function(){
+        $scope.needId = ($scope.selectedName === $scope.gateway[0]) ? false : true;
     }
+
+
+    var self = this;
+    self.customer = {  name:'',contact:'', email:'', selectedp:'', roomtype:'', roomno:'', checkIn:'', checkOut:'', guest_count:'', selectedName:'', booking_id:'', days:'', price:'', id_num:'' , extra:'', id_type:'', id_address:'',id_contact:''};
+    self.submit = submit;
+
+    function createBooking(guest){
+    	angularservice.createBooking(guest)
+            .then(
+            function(){
+                console.error('Error while creating contact');
+            }
+        );
+    }
+ 
+    function submit() {
+        console.log('Saving New Contact', self.customer);
+        createBooking(self.customer);
+    }
+
 }]);
 
 

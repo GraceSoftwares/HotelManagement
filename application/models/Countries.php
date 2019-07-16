ecb7301d2b47c1f549acdc48a55ddd126349e5ee
GraceSoftwares <51951120+GraceSoftwares@users.noreply.github.com>
2019-07-16 10:51:54 +0530

    Add files via upload

--- application/models/Countries.php
+++ application/models/Countries.php
@@ -9,10 +9,57 @@ class Countries extends CI_Model{
         return $this->db->count_all("tablebooking");
     }
 
-    public function fetch_data($limit, $start) {
-        $this->db->limit($limit, $start);
-        $query = $this->db->get("tablebooking");
+    //for dashboard table
+    public function fetch_data($tab) {
+        $date = new DateTime("now");
+        $curr_date = $date->format('Y-m-d ');
 
+//        $this->db->limit($limit, $start);
+        $this->db->from('tablebooking b'); 
+        $this->db->join('tableguest g','g.guestid = b.guestid');
+        $this->db->join('tableproperty p','p.propertyid=b.propertyid');
+        switch($tab){
+            case 'booking':$this->db->where('DATE(bookingdate)',$curr_date);break;
+            case 'checkin':$this->db->where('DATE(checkindate)',$curr_date);break;
+            case 'checkout':$this->db->where('DATE(checkoutdate)',$curr_date);break;
+        }
+        $this->db->where('DATE(bookingdate)',$curr_date);//use date function
+        $query = $this->db->get();
+
+        if ($query->num_rows() > 0) {
+            foreach ($query->result() as $row) {
+                $data[] = $row;
+            }
+            return $data;
+        }
+        return false;
+    }
+
+    // presenter
+    public function cust($id){
+        $this->db->from('tablebooking b');
+        $this->db->join('tableguest g','g.guestid=b.guestid');
+        $this->db->join('tableproperty p','p.propertyid=b.propertyid');
+        $this->db->where('b.id',$id);
+        $queryb = $this->db->get();
+        //$queryc = $this->db->get_where( 'tableproperty', array('propertyid' => $varb) );
+        if($queryb->num_rows() > 0){
+            return $queryb->row();
+        }
+        return false;
+    }
+
+    //report
+    public function getall($selector){
+        $this->db->from('tablebooking b');
+        $this->db->join('tableguest g','g.guestid=b.guestid');
+        $this->db->join('tableproperty p','p.propertyid=b.propertyid');
+        switch($selector){
+            case 'upcoming':$this->db->where('status', 'yet to come');break;
+            case 'ongoing':$this->db->where('(status ="in" or status = "out" )');break;
+            case 'completed':$this->db->where('status','vaccated');break;
+        }
+        $query = $this->db->get();
         if ($query->num_rows() > 0) {
             foreach ($query->result() as $row) {
                 $data[] = $row;
--- application/models/FormModel.php
+++ application/models/FormModel.php
@@ -8,18 +8,41 @@ class FormModel extends CI_Model {
     private $guest = 'tableguest';
     private $property = 'tableproperty';
 
-    function add_booking($id, $guest_id, $property_id, $customer_contact, $customer_checkIn, $customer_checkOut, $customer_price) {
-        $data = array('id' => $id, 'guestid' => $guest_id, 'propertyid' => $property_id, 'checkindate' => $customer_checkIn, 'checkoutdate' => $customer_checkOut, 'totalprice' => $customer_price, 'phone' => $customer_contact, 'status' => 'ACTIVE');
+    function add_booking($id, $guest_id, $property_id, $customer_name, $customer_days, $customer_gateway, $customer_contact, $customer_checkIn, $customer_checkOut, $customer_price){
+        $data = array(
+        'id' => $id, 
+        'guestid' => $guest_id, 
+        'propertyid' => $property_id, 
+        'name' => $customer_name, 
+        'checkindate' => $customer_checkIn, 
+        'checkoutdate' => $customer_checkOut,
+        'totalnights' => $customer_days,  
+        'totalprice' => $customer_price,
+        'gateway' => $customer_gateway, 
+        'bookedby' => 'Anonymous', 
+        'phone' => $customer_contact, 
+        'status' => 'ACTIVE');
         $this->db->insert($this->booking, $data);
     }
 
     function add_guest($guest_id, $customer_noofguest){
-        $data = array('guestid' => $guest_id, 'noofguest' => $customer_noofguest);
+        $data = array(
+        'guestid' => $guest_id, 
+        'noofguest' => $customer_noofguest);
         $this->db->insert($this->guest, $data);
     }
 
-    function add_property($property_id, $customer_price){
-        $data = array('propertyid' => $property_id, 'price' => $customer_price);
+    function add_property($property_id, $customer_property, $customer_roomtype, $customer_roomno, $customer_price, $customer_extra, $customer_cgstpct, $customer_cgstamt){
+        $data = array(
+        'propertyid' => $property_id,
+        'property'=>$customer_property,
+        'roomtype'=>$customer_roomtype,
+        'roomno' => $customer_roomno,
+        'price' => $customer_price,
+        'additionalprice' => $customer_extra,
+        'cgstpct' => $customer_cgstpct,
+        'cgstamt' => $customer_cgstamt
+        );
         $this->db->insert($this->property, $data);
     }
 
